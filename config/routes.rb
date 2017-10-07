2f95731192dbd309ca73e4ea6addd625856f9eac
nusound <uroshni77@gmail.com>
2017-10-07 09:42:41 +0200

    Sorted views for suplements

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
   resources :suplements
 
-  get 'suplements' => 'suplements#index'
-  get 'suplements/new' => 'suplements#new'
-  post 'suplements' => 'suplements#create'
-  get 'suplements/:id' => 'suplements#show'
-  get 'suplements/:id/edit' => 'suplements#edit'
-
   devise_for :users
   root to: "home#index"
 end
