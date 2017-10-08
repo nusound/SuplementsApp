178b36eb1abd1694f508b0ebc7c866a02f662f02
nusound <uroshni77@gmail.com>
2017-10-08 09:37:26 +0200

    Created Pages controller and implemented logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
   resources :suplements
 
-  devise_for :users
-  root to: "home#index"
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+  root to: "pages#home"
 end
