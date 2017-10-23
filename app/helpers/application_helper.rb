99560f76eeff3baec72f5cee60717ce707c02934
nusound <uroshni77@gmail.com>
2017-10-23 19:16:09 +0200

    Added background image and changed navbar color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,7 @@ module ApplicationHelper
     if current_user
       link_to 'Logout', destroy_user_session_path, method: :delete
     else
-      (link_to 'Register', new_user_registration_path) +
-      "<br>".html_safe +
+      (link_to 'Register', new_user_registration_path) + " or ".html_safe +
       (link_to 'Login', new_user_session_path)
     end
   end
