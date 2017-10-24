2ecc2ff97843c4c8640601216704166c8bbbb775
nusound <uroshni77@gmail.com>
2017-10-24 09:50:37 +0200

    Added create helper and fixed hamburger icon

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,16 @@ module ApplicationHelper
     if current_user
       link_to 'Logout', destroy_user_session_path, method: :delete
     else
-      (link_to 'Register', new_user_registration_path) + " or ".html_safe +
+      (link_to 'Register', new_user_registration_path) + " ".html_safe +
       (link_to 'Login', new_user_session_path)
+    end
+  end
+
+  def create_helper
+    if current_user
+      link_to "My Supplements", suplements_path(:user_id => current_user.id)
+    else
+      link_to "Create Your Supplement", new_suplement_path
     end
   end
 end
